96e5b2d3fffc0663c6b1b4cf57d3171fccea61bf
elCanarioVidal <pablo@umo.uy>
2017-10-02 12:21:54 -0300

    css ampliado
    Css listo menos -form

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>SmartLife</title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
      <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"> -->
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   
 
   </head>
   <body>
@@ -26,11 +28,11 @@
             </div>
             <div class="row">
               <div class="hero-btns">
-                <div class="col-xs-10  col-md-offset-2 col-md-4">
-                  <button type="submit" class="btn btn-default">ir a SMARTLIFE</button>
+                <div class="col-xs-offset-1 col-xs-10  col-md-offset-2 col-md-4">
+                 <a class="pull-right" href="http://smartlife.com.uy/">Ir a SMARTLIFE&nbsp;<svg class="lnr lnr-arrow-right-circle"><use xlink:href="#lnr-arrow-right-circle"> </use></svg></a>
                 </div>
-                <div class="col-xs-10 col-md-4">
-                  <button type="submit" class="btn btn-default">Ver premios</button>
+                <div class="col-xs-offset-1 col-xs-10 col-md-4">
+                  <a class="pull-right" href="#premios">Ver premios&nbsp;<svg class="lnr lnr-plus-circle"><use xlink:href="#lnr-arrow-down-circle"> </use></svg></a>
                 </div>
               </div>
             </div>
@@ -63,7 +65,9 @@
     </div>
     <div class="col-md-5">
       <!-- Inicio formulario-->
+      <div>
           <?php require_once("./html/sm-landing-form1.html") ?>
+      </div>    
        <!--Fin formulario-->
     </div>
   </div>
@@ -76,17 +80,18 @@
         <div class="col-md-12">
           <!--título info-->
             <div class="titulo-info">
-              <p>PREMIOS</p>
+              <h1><a name="premios"></a>PREMIOS</h1>
             </div>
           <!--Fin título info-->  
         </div>
       </div>
       <div class="row">
-        <div class="col-md-6">
-          <div class="row">
+        <div class="col-md-8">
+          <div class="row list-der">
             <div class="col-md-6">
+            <div class="lista-premios">
               <h4>PUNTA CANA</h4>
-              <p>PASAJE IDA Y VUELTA</p>
+              <p class="subt">PASAJE IDA Y VUELTA</p>
               <ul>
               <li>5 noches.</li>
               <li>2 adultos + 1 niño hasta 11 años.</li>
@@ -95,30 +100,34 @@
               <li>Viáticos USD 500.</li>
               </ul>
             </div>
+            </div>
             <div class="col-md-6">
+            <div class="lista-premios">
               <h4>DÍA ALL INCLUSIVE</h4>
               <ul>
               <li>Tarjeta con $ 60.000 disponibles para realizar el día de tus sueños</li>
               <li>Podrá utilizarse en cualquier comercio o servicio que acepte la misma</li>
               </ul>
             </div>
+            </div>
           </div>
           <div class="row">
             <div class="col-md-12">
+              <div class="relleno">
               <p>
-                Con la compra de cualquier producto Smartlife participás del sorteo de un premio All Inclusive a tu elección. Ver bases y condiciones en smartlifeallinclusive.com.uy a partir del 1/10/2017.
+                Con la compra de cualquier producto Smartlife <span class="resaltar">Durante el mes de octubre</span> participás del sorteo de un premio All Inclusive a tu elección. Ver bases y condiciones en smartlifeallinclusive.com.uy a partir del 1/10/2017.
               </p>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
           <div class="row">
             <div class="col-md-12">
               <div class="row">
                  <div class="col-md-12 col-sx-12">
+                 <div class="lista-premios">
                     <h4>CASA ALL INCLUSIVE</h4>
-                 </div>                
-                 <div class="col-md-6 col-sx-12">
                     <ul>
                       <li>Aire Acondicionado SL-ACFC09A</li>
                       <li>Ventilador SL-SF40W</li>
@@ -129,10 +138,6 @@
                       <li>Aspiradora SL-VC870216</li>
                       <li>Estufa Halógena SL-HH120</li>
                       <li>Plancha a Vapor SL-PL2371FF</li>
-                    </ul>
-                  </div>
-                  <div class="col-md-6 col-sx-12">
-                    <ul>
                       <li>Batidora SL-HM945B</li>
                       <li>Cafetera Espresso SL-CM4648VE</li>
                       <li>Exprimidor SL-JC5512</li>
@@ -140,7 +145,9 @@
                       <li>Juguera SL-JE5611</li>
                       <li>Licuadora SL-BL9295D</li>
                       <li>Sandwichera SL-SM6178</li>
-                    <li>Tostadora SL-TD8211 </li>
+                      <li>Tostadora SL-TD8211 </li>
+                  </ul>
+                 </div> 
                  </div>
               </div>
             </div>
@@ -157,14 +164,12 @@
   <div class="footer">
     <div class="container">
     <div class="row">
-      <div class="col-md-12">
-        <div class="btn-group pull-right"> 
-          <button class="btn btn-default" type="button">
-            <em class="fa fa-facebook-official"></em> Facebook
-          </button> 
-          <button class="btn btn-default" type="button">
-            <em class="fa fa-globe"></em> SitioWeb
-          </button> 
+      <div class="col-md-12 btn-pie">
+        <div class="col-md-2 pull-right"> 
+  <a class="pull-right" href="https://www.facebook.com/smartlife.uy/"><i class="fa fa-facebook-official"></i>&nbsp;Facebook</a>
+        </div>
+        <div class="col-md-2 pull-right"> 
+  <a class="pull-right" href="http://smartlife.com.uy/"><i class="fa fa-globe"></i>&nbsp;SMARTLIFE</a>
         </div>
       </div>
     </div>
@@ -173,7 +178,6 @@
   <!--Fin footer-->
 </div>
 </div>
-
 <div class="cabezal col-md-12 col-xs-12"></div>
 
     <!-- Optional JavaScript -->
@@ -183,6 +187,6 @@
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/ci.js"></script>
     <script type="text/javascript" src="/js/script.js"></script>
-
+    <script src="https://cdn.linearicons.com/free/1.0.0/svgembedder.min.js"></script>
   </body>
 </html>
