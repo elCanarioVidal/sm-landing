a6b7601e84d894dcf89347c8c5089647715679c8
elCanarioVidal <pablo@umo.uy>
2017-10-12 12:55:34 -0300

    arreglado mail
    Se arregl'o el contendio del mail

--- php/mail.php
+++ php/mail.php
@@ -1,3 +1,36 @@
-<?php echo $nombre ?>
-<?php echo $email ?>
-<?php echo $chances ?>
+<html>
+   <head>
+   <meta charset='utf-8'>
+   <title>Smartlifeallinclusive</title>
+   </head>
+   <body>
+   <table width='650' border='0' cellspacing='0' cellpadding='0' style="color: grey;">
+     <tr>
+       <td>
+       		<h1 style='font-family: Helvetica, Arial, sans-serif; text-align: center;'>¡Gracias por participar de <strong style="color: #4173b6">Smartlifeallinclusive</strong>!
+       		</h1>
+       </td>
+     </tr>
+     <tr>
+       <td><p style='font-family: Arial, sans-serif; text-align: center; font-size: 24px; margin: 0;'><?php echo $nombre ?></p>
+       	  <br>
+       	  <p style='font-family: Arial, sans-serif; text-align: center; font-size: 14px; margin: 0;'>Registraste tu compra con el siguiente email:<br>
+       	   <span style="color: #4173b6; font-weight: bolder;"><?php echo $email ?></span> 
+       	   <br>
+       	  y  generaste</p>
+       </td></span> 
+     </tr>
+     <tr>
+       <td align='center'>
+       	<p style='padding:20px; color:#4173b6; text-align: center; font-family: Arial, sans-serif; font-size: 50px; margin: 0;'><?php echo $chances ?></p>
+       	<p style='font-family: Arial, sans-serif; text-align: center; font-size: 14px; margin: 0;'>Chances</p>
+       </td>
+     </tr>
+   </table>
+   </body>
+  </html>
+
+
+
+
+
